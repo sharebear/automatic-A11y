98d4fcc1043f9c756b2b69f7bdf8fc23d0ea0fcc
Jonathan Share <jonny@sharebear.co.uk>
2022-09-06 10:33:01 +0200

    Exercise 3.2 ignore test errors

--- a11y-app/cypress/integration/a11y_spec.js
+++ a11y-app/cypress/integration/a11y_spec.js
@@ -1,10 +1,23 @@
 beforeEach(() => {
-    cy.visit('http://localhost:3000')
-    cy.injectAxe()
-  })
+  cy.visit("http://localhost:3000");
 
-describe('Todo app', () => {
-    it('Should be accessible', () => {
-        cy.checkA11y()
-    })
-  })
+  cy.injectAxe();
+  cy.configureAxe({
+    rules: [
+      {
+        id: "landmark-one-main",
+        enabled: false,
+      },
+      {
+        id: "page-has-heading-one",
+        enabled: false,
+      },
+    ],
+  });
+});
+
+describe("Todo app", () => {
+  it("Should be accessible", () => {
+    cy.checkA11y();
+  });
+});
